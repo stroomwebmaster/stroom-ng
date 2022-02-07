e86151ff7df947fe194ef0471d1fefd9aea14c98
Gerwin Overeem <gerwin@overeem.io>
2022-02-07 12:29:26 +0100

    Update content

--- src/components/upcoming/index.js
+++ src/components/upcoming/index.js
@@ -59,9 +59,7 @@ export default class Upcoming extends Component {
 							<iframe id="youtubevideo" src={this.state.video} class={`w-full h-full  ${this.state.isPlaying ? '' : 'hidden'}`} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; modestbranding" allowfullscreen />
 						</div>
 						<div class="col-span-4 md:col-start-6 md:col-span-3 lg:col-start-10 lg:col-span-3">
-							<p class="text-sm md:text-base lg:text-2xl leading-relaxed mt-8 md:mt-0 mb-4 md:mb-16 text-white font-HelveticaNeueLTPro-Ex">
-								{this.props.content?.description}
-							</p>
+							<p class="text-sm md:text-base lg:text-2xl leading-relaxed mt-8 md:mt-0 mb-4 md:mb-16 text-white font-HelveticaNeueLTPro-Ex" innerHTML={this.props.content?.description} />
 							<br />
 							{linkItems}
 						</div>

fbbf42c4d1d960b171e937c0e47e30e3dff1ba3c
Bart Guijt <bart@guijt.me>
2022-04-26 21:54:10 +0200

    Initial MD support

--- src/components/venue/index.js
+++ src/components/venue/index.js
@@ -1,6 +1,7 @@
 import { Component } from 'preact';
 
 import style from './style';
+import Markdown from 'preact-markdown';
 
 export default class Venue extends Component {
     componentDidMount() {
@@ -13,10 +14,12 @@ export default class Venue extends Component {
             <article class="bg-coal w-hd-80 xl:ml-auto relative">
                 <img src="./assets/dekas.jpg" class={`w-full ${style.locationPhoto}`} />
                 <div class="container mx-auto relative -top-12">
-                    <div class="col-span-4 md:col-span-8 lg:col-span-12 grid md:grid-cols-8 lg:grid-cols-12 bg-orange lg:mt--40 border-b-8 border-turquoise py-8 md:py-12">
-                        <p class="text-coal text-sm md:text-lg lg:text-2xl leading-relaxed md:leading-relaxed lg:leading-relaxed md:mb-8 font-HelveticaNeueLTPro-Ex pl-4 whitespace-pre-line">
-                            {this.props.content?.description}
-                        </p>
+                    <div class="grid md:grid-cols-8 lg:grid-cols-12 bg-orange lg:mt--40 border-b-8 border-turquoise py-8 md:py-12">
+                        <div class="col-span-4 md:col-span-8 lg:col-span-10 lg:col-start-2">
+                            <p class="text-coal text-sm md:text-lg lg:text-2xl leading-relaxed md:leading-relaxed lg:leading-relaxed font-HelveticaNeueLTPro-Ex pl-4 whitespace-pre-line">
+                                <Markdown markdown={this.props.content?.description} />
+                            </p>
+                        </div>
                     </div>
                 </div>
             </article>

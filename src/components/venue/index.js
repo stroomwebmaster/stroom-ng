2741627becd3532936975fe6ab85e3ac51ac0cc2
Bart Guijt <bart@guijt.me>
2022-04-26 21:03:55 +0200

    Progress

--- src/components/venue/index.js
+++ src/components/venue/index.js
@@ -10,11 +10,13 @@ export default class Venue extends Component {
 
     render() {
         return (
-            <article class="bg-coal w-hd-80 xl:ml-auto">
+            <article class="bg-coal w-hd-80 xl:ml-auto relative">
                 <img src="./assets/dekas.jpg" class={`w-full ${style.locationPhoto}`} />
-                <div class="container mx-auto">
+                <div class="container mx-auto relative -top-12">
                     <div class="col-span-4 md:col-span-8 lg:col-span-12 grid md:grid-cols-8 lg:grid-cols-12 bg-orange lg:mt--40 border-b-8 border-turquoise py-8 md:py-12">
-                        {this.props.content?.description}
+                        <p class="text-coal text-sm md:text-lg lg:text-2xl leading-relaxed md:leading-relaxed lg:leading-relaxed md:mb-8 font-HelveticaNeueLTPro-Ex pl-4 whitespace-pre-line">
+                            {this.props.content?.description}
+                        </p>
                     </div>
                 </div>
             </article>

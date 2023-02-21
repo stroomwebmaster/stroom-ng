45453829aa2fed36cb90c7e31b45d572f5d1b3ce
Bart Guijt <bart@guijt.me>
2023-02-21 23:12:23 +0100

    Improved gap between videos when smaller screen

--- src/components/aboutvideo/index.js
+++ src/components/aboutvideo/index.js
@@ -22,7 +22,7 @@ export default class AboutVideo extends Component {
 
 	render(props, state, context) {
 		return (
-			<div class={`col-span-3 bg-black w-9_16 md:h-9_16md lg:h-9_16lg h-9_16 even:mt-12 shadow-sharp-${props.shadow}`}>
+			<div class={`col-span-3 bg-black w-9_16 md:h-9_16md lg:h-9_16lg h-9_16 even:lg:mt-12 odd:lg:mt-0 mt-6 shadow-sharp-${props.shadow}`}>
 				<iframe id={`aboutvideo${props.content}`} src={state.video} class="w-full h-full" frameBorder="0"
 						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; modestbranding"
 						allowFullScreen />

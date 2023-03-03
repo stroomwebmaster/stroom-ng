3c7e38a744166eedcaab68f9ec10093f8b340960
Gerwin Overeem <gerwin@overeem.io>
2023-03-03 20:25:35 +0100

    fix: Activities layout

--- src/components/activities/index.js
+++ src/components/activities/index.js
@@ -1,8 +1,8 @@
 import Activity from './activity';
 
 const Activities = (props) => (
-	<article class="container mx-auto relative">
-		<div class="px-4 md:px-0 mb-20 -mt-24 md:-mt-36 md:mb-32 lg:mb-40">
+	<article class="relative xl:ml-auto w-hd-80">
+		<div class="container mx-auto px-4 md:px-0 mb-20 -mt-24 md:-mt-36 md:mb-32 lg:mb-40">
 			<h2 class="text-xl md:text-3xl lg:text-4xl font-black uppercase text-red my-3 lg:my-6 font-HelveticaNeueLTPro-BlkEx">{props.content?.title}</h2>
 			<div class="grid grid-cols-4 md:grid-cols-8 lg:grid-cols-12 gap-4">
 				<div class="col-span-4 lg:col-span-6 hidden md:block">

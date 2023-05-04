0253ef32a721f8e77850c63f80cc668015fa3b42
Gerwin Overeem <gerwin@overeem.io>
2023-05-04 21:33:33 +0200

    fix: FAQ title

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -51,7 +51,7 @@ export default class Home extends Component {
 				
 							<div class="col-span-4 md:col-span-8 lg:col-span-12 grid md:grid-cols-8 lg:grid-cols-12 bg-orange mt-40 lg:mt-64 border-b-8 border-turquoise py-8 md:py-12">
 								<div class="col-span-4 md:col-span-7 lg:col-span-11 md:col-start-2 lg:col-start-2 px-6 md:px-0">
-									<h2 class="text-xl md:text-3xl lg:text-4xl font-black uppercase text-red my-6 font-HelveticaNeueLTPro-BlkEx">{this.props.content?.faq?.title}</h2>
+									<h2 class="text-xl md:text-3xl lg:text-4xl font-black uppercase text-red my-6 font-HelveticaNeueLTPro-BlkEx">{this.props?.faq?.title}</h2>
 									<ul class="mb-10">
 										{faqItems}
 									</ul>

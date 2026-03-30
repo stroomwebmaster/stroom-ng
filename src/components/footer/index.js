b4aefa37ae669188cdc913cb46fa943ac368b432
Gerwin Overeem <gerwin@overeem.io>
2026-03-30 21:17:41 +0200

    fix: use native link for a element in md

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -17,7 +17,7 @@ export default class Home extends Component {
 				(<>
 					<li class="mb-6">
 						<h2 class="uppercase text-red text-base md:text-lg lg:text-2xl font-HelveticaNeueLTPro-MdCn">{faq.question}</h2>
-						<span class="text-sm md:text-base lg:text-xl font-HelveticaNeueLTPro-Ex underlined-links-parent"><Markdown>{faq.answer}</Markdown></span>
+						<span class="text-sm md:text-base lg:text-xl font-HelveticaNeueLTPro-Ex underlined-links-parent"><Markdown options={{ overrides: { a: { component: 'a', props: { native: true } } } }}>{faq.answer}</Markdown></span>
 					</li>
 				</>)
 			);

21fd1b4554b6a610e4dd47e84c33e2ed38d7ab32
Gerwin Overeem <gerwin@overeem.io>
2026-03-30 21:29:49 +0200

    feat: spacing added

--- src/routes/visie/index.js
+++ src/routes/visie/index.js
@@ -15,7 +15,7 @@ const Visie = (props) => {
 				<div class="container mx-auto">
 					<div class="px-4 md:px-0">
 						<div class="grid grid-cols-4 md:grid-cols-8 lg:grid-cols-12 gap-4 relative">
-							<div class="col-span-4 md:col-start-2 lg:col-start-2 md:col-span-6 lg:col-span-8 my-12 md:mt-24 md:mb-20 lg:mb-40">
+							<div class="col-span-4 md:col-start-2 lg:col-start-2 md:col-span-6 lg:col-span-8 my-12 md:mt-24 mb-20 lg:mb-40">
 								<div class="text-coal text-sm md:text-base lg:text-xl leading-relaxed md:leading-relaxed lg:leading-relaxed font-HelveticaNeueLTPro-Ex pl-4 markup">
 									<Markdown>{prerender.data.body?.content || ''}</Markdown>
 								</div>
